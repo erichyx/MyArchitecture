cebb51ffcbee90ce172bbb7421b7fe5ee9b2dc24
erichyx <erichyx@yeah.net>
2018-06-16 14:21:17 +0800

    删减冗余代码。

--- basicore/src/main/java/cn/eric/basicore/arch/mvp/presenter/BaseSimpleMvpPresenter.java
+++ basicore/src/main/java/cn/eric/basicore/arch/mvp/presenter/BaseSimpleMvpPresenter.java
@@ -1,12 +1,7 @@
 package cn.eric.basicore.arch.mvp.presenter;
 
-import android.arch.lifecycle.Lifecycle;
-import android.arch.lifecycle.LifecycleObserver;
-import android.arch.lifecycle.OnLifecycleEvent;
-
 import cn.eric.basicore.arch.mvp.view.BaseMvpView;
 import io.reactivex.SingleObserver;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 
 /**
@@ -15,8 +10,6 @@ import io.reactivex.disposables.Disposable;
 
 public abstract class BaseSimpleMvpPresenter<V extends BaseMvpView, T> extends BaseMvpPresenter<V>
         implements SingleObserver<T> {
-
-    private CompositeDisposable mDisposable = new CompositeDisposable();
 
     @Override
     public void onSubscribe(Disposable d) {
